e768be627b92190ada6b6fa85149f6aa219485e2
RogorStuff <lacruzrogelio@gmail.com>
2020-03-19 13:31:57 +0100

    Avance de regostro

--- identification.js
+++ identification.js
@@ -1,5 +1,6 @@
-var register_path = "https://ps-20-server-django-app.herokuapp.com/api/v1/rest-auth/registration/";
+var register_path = "https://ps-20-server-django-app.herokuapp.com/api/v1/rest-auth/registration";
 
+//Comprueba mediante una expresión regular que el mensaje por parámetro sea un email
 function ValidateEmail(inputText)
 {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -13,9 +14,11 @@ function ValidateEmail(inputText)
     }
 }
 
+//Función para comprobar que todos los campos del formulario son válidos, y para el registro del usuario
 function validacionRegister() {
     document.getElementById("userError").style.visibility="hidden";
     document.getElementById("emailError").style.visibility="hidden";
+    document.getElementById("pass1Error").style.visibility="hidden"; 
     var User = document.getElementById("username"); 
     var Email = document.getElementById("email"); 
     var Password1 = document.getElementById("password1"); 
@@ -23,52 +26,91 @@ function validacionRegister() {
     User.style.border="none"
     Email.style.border="none"
     Password1.style.border="none"
+    Password2.style.border="none"
 
     if(User.value.trim() == ""){
-        User.style.border="solid 2px red"
-        document.getElementById("userError").innerHTML="Username can't be an empty value";
-        document.getElementById("userError").style.visibility="visible";
+        User.style.border="solid 2px red";
+        document.getElementById("userRError").innerHTML="Username can't be an empty string";
+        document.getElementById("userRError").style.visibility="visible";
         return false;
     }else if(ValidateEmail(Email)===false){
-        Email.style.border="solid 2px red"
+        Email.style.border="solid 2px red";
         document.getElementById("emailError").innerHTML="Invalid email";
         document.getElementById("emailError").style.visibility="visible";
         return false;
     }else if(Password1.value.trim() == "" || Password2.value.trim() == ""){
-        Password1.style.border="solid 2px red"
-        document.getElementById("passError").innerHTML="Passwords can't be an empty value";
-        document.getElementById("passError").style.visibility="visible";
+        Password1.style.border="solid 2px red";
+        Password2.style.border="solid 2px red";
+        document.getElementById("pass1Error").innerHTML="Passwords can't be an empty value";
+        document.getElementById("pass1Error").style.visibility="visible";
         return false;
     }else if(Password1.value.trim() != Password2.value.trim()){
-        Password1.style.border="solid 2px red"
-        document.getElementById("passError").innerHTML="Passwords must be the same";
-        document.getElementById("passError").style.visibility="visible";
+        Password1.style.border="solid 2px red";
+        Password2.style.border="solid 2px red";
+        document.getElementById("pass1Error").innerHTML="Passwords must be the same";
+        document.getElementById("pass1Error").style.visibility="visible";
         return false;
     }else{
-        return true;
+        alert("Empiezo");
+
+        var data = {
+            'username': User.value,
+            'email': Email.value,
+            'password1': Password1.value,
+            'password2': Password2.value,
+        };
+
+        var form = new FormData(document.getElementById("register-form"))
+
+        fetch(register_path,{
+            method: "POST",
+            body=form
+        })
+            .then(function(response) {
+            if (response.ok) {
+               // alert(response.json());
+                return response.json();
+            } else {
+                alert("falloApi");
+                throw new Error("Could not reach the API: " + response.statusText);
+            }
+        alert("fetch acaba?");
+        }).then(function(data) {
+            document.getElementById("pass1Error").innerHTML = data.encoded;
+        }).catch(function(error) {
+            document.getElementById("pass1Error").innerHTML = error.message;
+        });
+        alert("Acabo");
+
+
+        return false;
     }
 }
 
+//Función para comprobar que todos los campos del formulario son válidos, y para el login del usuario
 function validacionLogin() {
-    var UserEmail = document.getElementById("username"); 
-    var Email = document.getElementById("email"); 
-    var Password1 = document.getElementById("password1");
+    document.getElementById("userError").style.visibility="hidden";
+    document.getElementById("passError").style.visibility="hidden";
+    var UserEmail = document.getElementById("usernameEmail"); 
+    var Password = document.getElementById("password");
+    UserEmail.style.border="none";
+    Password.style.border="none";
 
-    if (User.value.trim() == ""){
-        User.style.border="solid 3px red"
+    if (UserEmail.value.trim() == ""){
+        User.style.border="solid 3px red";
+        document.getElementById("userError").innerHTML="This field can't be an empty parameter";
         document.getElementById("userError").style.visibility="visible";
         return false;
-    }else if(ValidateEmail(Email)){
-        Email.style.border="solid 3px red"
-        document.getElementById("emailError").style.visibility="visible";
+    }else if(Password.value.trim()  == ""){
+        User.style.border="solid 3px red";
+        document.getElementById("passError").innerHTML="The password can't be empty";
+        document.getElementById("passError").style.visibility="visible";
         return false;
-    }else if((Password1.value.trim() != Password2.value.trim()) || Password1.value.trim() == "" || Password2.value.trim() == ""){
-        alert("Error en las passwords");
-        return false;
-    }else if((Password1.value.trim() != Password2.value.trim()) || Password1.value.trim() == "" || Password2.value.trim() == ""){
-        alert("Error en las passwords");
+    }else if(ValidateEmail(UserEmail)){
+        //El usuario introduce un email
         return false;
     }else{
-        return true;
+        //El usuario introduce un nombre de usuario
+        return false;
     }
 }

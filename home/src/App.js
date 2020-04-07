61e5758164e4bc85cbb480e4e7201e44cac24835
RogorStuff <lacruzrogelio@gmail.com>
2020-04-07 16:17:55 +0200

    Implementacion de diseño (falta dropwdown bar y quitar boton clear audio, uso debug)

--- home/src/App.js
+++ home/src/App.js
@@ -21,7 +21,7 @@ class App extends Component{
 
   render(){
   return(
-    <div className="h-100">
+    <div className="box">
       <nav className="navbar navbar-expand-md navbar-dark custom-navbar">
         <div className="col-xl-4 navbar-header">
           <img className="img-fluid mx-auto" src={logo} alt="logo" style={{maxHeight: 75}}></img>
@@ -31,36 +31,54 @@ class App extends Component{
         </div>
         <div className="col-xl-8 collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
           <ul className="nav navbar-nav">
+            <form className="form-inline">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+              <p className="readable-text">Dropdown por poner</p>
+              <button className="form_button" type="submit">Search</button>
+            </form>
+
             <button onClick={this.cambiaSource}>
             Clear audio player
             </button>
           </ul>
         </div> 
       </nav>
-
-      <div className="container-fluid h-100 d-inline-block">
-        <div class="row">
-          <div class="col-3 hidden-md-down" id="yellow">Prueba
+      
+      <div className="container-fluid content">
+        <div class="row content full-height">
+          <div class="col-sm-3 menu-appearance border border-dark border-right content"  style={{paddingTop:10}}>
+            <ul style={{listStyleType:'none',padding: 0}}>
+              <li className="distance readable-text">Playlists</li>
+              <li className="distance readable-text">Podcasts</li>
+              <li className="hr"></li>
+              <li className="distance readable-text">Friends</li>
+              <li className="hr"></li>
+              <li className="distance readable-text">My account</li>
+              <li className="distance readable-text">Log out</li>
+            </ul>
           </div>
-          <div class="col-9">Prueba
+          <div class="col-sm-9 full-height">Contenido
           </div>
         </div>
       </div>
-
+      
       <footer className="footer fixed-bottom custom-navbar">
-        <div className="row">
-          <div className="col-sm-3 justify-content-center text-center">
-            <div>{this.state.title}</div>
-            <div>{this.state.author}</div>
-            <div>{this.state.album}</div>
-            <div>{this.state.rating}</div>
+          <div className="row">
+            <div className="col-sm-1 readable-text">Aquí imagen</div>
+            <div className="col-sm-3 justify-content-center text-center">
+              <div className="readable-text">{this.state.title}</div>
+              <div className="readable-text">{this.state.author}</div>
+              <div className="readable-text">{this.state.album}</div>
+              <div className="readable-text">{this.state.rating}</div>
+            </div>
+            <div className="col-sm-8">
+              <AudioPlayer className="full-height" src={this.state.src}></AudioPlayer>  
+            </div>
           </div>
-          <div className="col-sm-8">
-            <AudioPlayer className="full-height" src={this.state.src}></AudioPlayer>  
-          </div>
-        </div>
+          <div className="darker-bar" style={{height:25}}></div>
       </footer>
     </div>
+
   );
 }
 

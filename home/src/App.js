adabe98e751d59ce351a848738ff1765951b44a1
Fran-sw <61191367+Fran-sw@users.noreply.github.com>
2020-05-09 16:15:30 +0200

    Bug Fixes

--- home/src/App.js
+++ home/src/App.js
@@ -1070,6 +1070,8 @@ class App extends Component{
             title: newSrc.title,
             author: newSrc.album.artist.name,
             album: newSrc.album.name,
+            rating: newSrc.avg_valoration,
+            userRated: newSrc.user_valoration,
             playingPlaylist:1,
           });
           if(newSrc.user_valoration){
@@ -1174,6 +1176,8 @@ class App extends Component{
             title: newSrc.title,
             author: this.state.podcastAuthor,
             album: this.state.openPodcast,
+            rating: newSrc.avg_valoration,
+            userRated:  newSrc.user_valoration,
             playingPlaylist:1,
           });
           if(newSrc.user_valoration){
@@ -1217,6 +1221,8 @@ class App extends Component{
                 title: sortedSongs[0].title,
                 author: datosPd.artist.name,
                 album: response.name,
+                rating:  sortedSongs[0].avg_valoration,
+                userRated:   sortedSongs[0].user_valoration,
                 playingPlaylist:1,
               });
               if(newSrc.user_valoration){
@@ -1243,6 +1249,8 @@ class App extends Component{
               title: this.state.openPlaylist[0].title,
               author: this.state.podcastAuthor,
               album: this.state.openPodcast,
+              rating: this.state.avg_valoration,
+              userRated:  this.state.user_valoration,
               playingPlaylist:1,
             });
             if(newSrc.user_valoration){
